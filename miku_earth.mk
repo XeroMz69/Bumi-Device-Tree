563f971c84aab2ca434bea6160d35e25e2c15213
Adam YD <adamyd18@gmail.com>
2024-08-29 09:30:49 +0700

    vendor: Adding miku vendor

--- miku_earth.mk
+++ miku_earth.mk
@@ -15,7 +15,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 PRODUCT_NAME := miku_earth
 PRODUCT_DEVICE := earth
