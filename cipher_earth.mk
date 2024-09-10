e195c9cf6373b1db5f8691a8baf7c64791b4a874
Adam YD <adamyd18@gmail.com>
2024-09-10 08:04:51 +0700

    earth: Bringup CipherOS

--- cipher_earth.mk
+++ cipher_earth.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_earth
 PRODUCT_DEVICE := earth
