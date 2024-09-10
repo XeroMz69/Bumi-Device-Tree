80f3eeee67ec2695aff5c9a8f253e4ce7f4b19ce
Adam YD <adamyd18@gmail.com>
2024-09-10 19:27:34 +0700

    earth: Bringup RisingOS

--- lineage_earth.mk
+++ lineage_earth.mk
@@ -13,6 +13,7 @@ $(call inherit-product, device/xiaomi/earth/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+RISING_MAINTAINER := xeromz
 
 PRODUCT_NAME := lineage_earth
 PRODUCT_DEVICE := earth
@@ -24,5 +25,19 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="earth-user 14 UP1A.230105.007 V816.0.2.0.UCVMIXM release-keys"
+    RISING_CHIPSET="Mediatek Helio G85" 
+    RISING_MAINTAINER="Xero"
 
 BUILD_FINGERPRINT := Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys
+
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+RISING_PACKAGE_TYPE := "VANILLA_AOSP"
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := false
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
+WITH_GMS := false
+TARGET_CORE_GMS := false
+TARGET_CORE_GMS_EXTRAS := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
