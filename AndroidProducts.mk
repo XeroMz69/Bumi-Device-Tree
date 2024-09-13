8cbb2de862d28a1de65b44e1517b8910a7dd4552
Adam YD <adamyd18@gmail.com>
2024-09-14 06:07:14 +0700

    earth: Bringup AfterlifeOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_earth.mk
+    $(LOCAL_DIR)/afterlife_earth.mk
+
+COMMON_LUNCH_CHOICES := \
+    afterlife_earth-user \
+    afterlife_earth-userdebug \
+    afterlife_earth-eng
