c0673800c949b1c1355766a80939db4f99396696
Adam YD <adamyd18@gmail.com>
2024-09-21 09:48:11 +0700

    earth: Bringup Cherish

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_earth.mk
+    $(LOCAL_DIR)/cherish_earth.mk
+
+COMMON_LUNCH_CHOICES := \
+    cherish_earth-user \
+    cherish_earth-userdebug \
+    cherish_earth-eng
