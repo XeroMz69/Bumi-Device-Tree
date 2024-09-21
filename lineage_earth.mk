f324bf87003a6b0b94b28fafe8f310185de6f7cf
Adam YD <adamyd18@gmail.com>
2024-09-21 22:20:52 +0700

    earth:  Make the ROM vanilla

--- lineage_earth.mk
+++ lineage_earth.mk
@@ -13,7 +13,6 @@ $(call inherit-product, device/xiaomi/earth/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-RISING_MAINTAINER := xeromz
 
 PRODUCT_NAME := lineage_earth
 PRODUCT_DEVICE := earth
@@ -24,8 +23,8 @@ PRODUCT_MODEL := 22120RN86C
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="earth-user 14 UP1A.230105.007 V816.0.2.0.UCVMIXM release-keys"
-    RISING_CHIPSET="Mediatek Helio G85" 
+    PRIVATE_BUILD_DESC="earth-user 14 UP1A.230105.007 V816.0.2.0.UCVMIXM release-keys" \
+    RISING_CHIPSET="MT6768" \
     RISING_MAINTAINER="Xero"
 
 BUILD_FINGERPRINT := Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys
@@ -33,12 +32,14 @@ BUILD_FINGERPRINT := Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVM
 # Charging Animation
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
-# GAPPS
+# Rising OS
+RISING_PACKAGE_TYPE := VANILLA_AOSP
+TARGET_ENABLE_BLUR := false
+PRODUCT_NO_CAMERA := true
 WITH_GMS := false
-TARGET_CORE_GMS := true
+TARGET_CORE_GMS := false
 TARGET_CORE_GMS_EXTRAS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
-RISING_PACKAGE_TYPE := "CORE_AOSP"
 TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
 TARGET_DEFAULT_PIXEL_LAUNCHER := true
 TARGET_BOOT_ANIMATION_RES := 720
