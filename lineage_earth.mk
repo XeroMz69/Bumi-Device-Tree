9eebf416da1a66e5490e55c7cfa1a9a1d70b26fd
Adam YD <adamyd18@gmail.com>
2024-11-18 08:23:22 +0000

    earth: DroidX

--- lineage_earth.mk
+++ lineage_earth.mk
@@ -12,9 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_earth
+DROIDX_BUILD_TYPE := COMMUNITY
+DROIDX_GAPPS := false
+
+PRODUCT_NAME := droidx_earth
 PRODUCT_DEVICE := earth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
