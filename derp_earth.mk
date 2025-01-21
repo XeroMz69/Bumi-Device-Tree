cb401cb02ad1360d22172e0d1ce1674fa8cadf39
XeroMz69 <adamyd18@gmail.com>
2025-01-21 20:11:56 +0700

    earth: Derp it
    Signed-off-by: XeroMz69 <adamyd18@gmail.com>

---
+++ derp_earth.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/earth/device.mk)
+
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Some flags
+TARGET_BOOT_ANIMATION_RES := 720
+DERP_BUILDTYPE := Community
+TARGET_USES_CORE_GAPPS := true
+
+PRODUCT_NAME := derp_earth
+PRODUCT_DEVICE := earth
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 22120RN86C
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="earth-user 14 UP1A.230105.007 V816.0.2.0.UCVMIXM release-keys" \
+    BuildFingerprint=Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys \
+    DeviceName=earth \
+    DeviceProduct=earth \
+    SystemDevice=earth \
+    SystemName=earth
