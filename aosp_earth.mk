b0f693bf79347dfa216c6ba8f801e39ceb348a43
XeroMz69 <adamyd18@gmail.com>
2025-03-31 06:17:48 +0000

    earth: Initial AOSP Tree
    Signed-off-by: XeroMz69 <adamyd18@gmail.com>

---
+++ aosp_earth.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2023-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/earth/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Boot Animaton
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Pixel Charger
+USE_PIXEL_CHARGER := true
+
+PRODUCT_NAME := lineage_earth
+PRODUCT_DEVICE := earth
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 22120RN86C
+
+PRODUCT_SYSTEM_NAME := earth_global
+PRODUCT_SYSTEM_DEVICE := earth
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="earth-user 14 UP1A.230105.007 V816.0.7.0.UCVMIXM release-keys" \
+    BuildFingerprint=Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.7.0.UCVMIXM:user/release-keys
+    SystemModel=$(PRODUCT_SYSTEM_DEVICE) \
+    SystemName=$(PRODUCT_SYSTEM_NAME) \
+    ProductModel=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
