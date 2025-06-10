ebaa472eeef33645c8e1e8a31ad66b1e0d74da6c
XeroMz69 <adamyd18@gmail.com>
2025-06-10 17:41:50 +0700

    Revert "fixup! earth: Build missing libraries for 14 QPR3"
    This reverts commit 8a93a6a73c5d73d8f1df6551fe1962b908b07cd6.
    
    Signed-off-by: XeroMz69 <adamyd18@gmail.com>

--- device.mk
+++ device.mk
@@ -382,8 +382,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.0.vendor \
-    android.hardware.tetheroffload.control@1.1.vendor \
-    libnetutils.vendor
+    android.hardware.tetheroffload.control@1.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
