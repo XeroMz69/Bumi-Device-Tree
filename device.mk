d3d774bd5ad83d755294aacd89ef09bbbb1a7230
XeroMz69 <adamyd18@gmail.com>
2025-06-10 21:42:11 +0700

    Revert "earth: Update makefile to use the AIDL Wifi Vendor HAL."
    This reverts commit 7e0e92e7150145892db0ce00eb032c13c495c042.
    
    Signed-off-by: XeroMz69 <adamyd18@gmail.com>

--- device.mk
+++ device.mk
@@ -366,7 +366,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     wpa_supplicant \
     hostapd \
-    android.hardware.wifi-service
+    android.hardware.wifi@1.0-service-lazy
 
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
