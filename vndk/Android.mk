d7d8780c8cc4d58c295bd86b25eda7e9a8e8c99c
XeroMz69 <adamyd18@gmail.com>
2025-06-11 15:25:34 +0700

    earth: Nuke `libstagefright_foundation-v33` definition
    * https://github.com/LineageOS/android_hardware_lineage_compat/commit/250739971e4ff8a92a71f088432960d2c0edc4a1
    
    Change-Id: I4a219c8cf54f0e9ee563f4fcd60d2194cc2affe2
    Signed-off-by: XeroMz69 <adamyd18@gmail.com>

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -10,16 +10,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CHECK_ELF_FILES := false
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libstagefright_foundation-v33
-LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_arm := $(call my-dir)/v33/arm64/libstagefright_foundation.so
-LOCAL_SRC_FILES_arm64 := $(call my-dir)/v33/arm64/libstagefright_foundation.so
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TARGET_ARCH := arm arm64
-LOCAL_MODULE_TAGS := optional
-LOCAL_CHECK_ELF_FILES := false
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
