954beebd68571feab6741cb9483ea1525da0b620
XeroMz69 <adamyd18@gmail.com>
2025-06-11 19:56:43 +0700

    earth: vndk: Add `LOCAL_PATH`
    Signed-off-by: XeroMz69 <adamyd18@gmail.com>

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,8 +1,10 @@
+LOCAL_PATH := prebuilts/vndk
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libutils-v32
 LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_arm := prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so
-LOCAL_SRC_FILES_arm64 := prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so
+LOCAL_SRC_FILES_arm := v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so
+LOCAL_SRC_FILES_arm64 := v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TARGET_ARCH := arm arm64
